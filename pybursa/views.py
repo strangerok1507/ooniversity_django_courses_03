869f21dcf394274497157b69396a26ffde4bb0f5
Vlad Zhirov <zhirov0vlad@gmail.com>
2015-11-19 23:07:07 +0200

    add templates and static files

---
+++ pybursa/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+def index(request):
+    return render(request,'index.html')
+    
+def contact(request):
+    return render(request,'contact.html')
+
+def student_list(request):
+    return render(request,'student_list.html')
+
+def student_detail(request):
+    return render(request,'student_detail.html')
