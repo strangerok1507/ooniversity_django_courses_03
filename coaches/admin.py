73be2584627da6fcab3ea61bd28846fd9984fc92
Vlad Zhirov <zhirov0vlad@gmail.com>
2015-11-29 14:57:46 +0200

    Add coaches

---
+++ coaches/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from coaches.models import Coach
+from django.contrib.auth.models import User
+
+class CoachAdmin(admin.ModelAdmin):
+  list_display = ('first_name', 'last_name', 'gender', 'skype', 'description')
+  list_filter = ['user__is_staff']
+
+admin.site.register(Coach,CoachAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+from coaches.models import Coach
 
 class Course(models.Model):
-  name = models.CharField(verbose_name=u'Name of course', max_length=255)
-  short_description = models.CharField(max_length=255)
+  name = models.CharField(verbose_name=u'Name of course', max_length = 255)
+  short_description = models.CharField(max_length = 255)
   description = models.TextField()
+  coach = models.ForeignKey(Coach, blank=True, null=True, related_name="coach_courses")
+  assistant = models.ForeignKey(Coach, blank=True, null=True, related_name="assistant_courses")
   
   def __unicode__(self):
     return self.name
 
 class Lesson(models.Model):
-  subject = models.CharField(max_length=255)
+  subject = models.CharField(max_length = 255)
   description = models.TextField()
   course = models.ForeignKey(Course) 
   order = models.PositiveIntegerField()
