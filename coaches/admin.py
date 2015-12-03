dc744cf9d887540eb679c8db42fcda7b2d6ea3a6
Vlad Zhirov <zhirov0vlad@gmail.com>
2015-12-03 09:16:19 +0200

    refactor html

--- coaches/admin.py
+++ coaches/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from coaches.models import Coach
 from django.contrib.auth.models import User
 
+
 class CoachAdmin(admin.ModelAdmin):
   list_display = ('first_name', 'last_name', 'gender', 'skype', 'description')
   list_filter = ['user__is_staff']
--- courses/admin.py
+++ courses/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from courses.models import Course,Lesson
 
 
-  
-
 class LessonInline(admin.TabularInline):
   model = Lesson
   extra = 0
