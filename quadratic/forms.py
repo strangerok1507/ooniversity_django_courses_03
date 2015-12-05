7b3e066f3e59e8b5125b14004ea68e2b786e905e
Vlad Zhirov <zhirov0vlad@gmail.com>
2015-12-05 12:22:03 +0200

    change label text

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -3,9 +3,9 @@ from django import forms
 
 
 class QuadraticForm(forms.Form):
-  a = forms.IntegerField(label=u'Коеффициент а')
-  b = forms.IntegerField(label=u'Коеффициент b')
-  c = forms.IntegerField(label=u'Коеффициент с')
+  a = forms.IntegerField(label="коэффициент a")
+  b = forms.IntegerField(label="коэффициент b")
+  c = forms.IntegerField(label="коэффициент c")
 
 
   def clean_a(self):
