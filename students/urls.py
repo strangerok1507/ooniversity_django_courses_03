9990cec2d94c85252e2c13edcb1b7c26f4837f7c
Vlad Zhirov <zhirov0vlad@gmail.com>
2015-12-05 18:02:29 +0200

    change text for the views.py

--- students/urls.py
+++ students/urls.py
@@ -4,7 +4,7 @@ from students import views
 urlpatterns = patterns('',
     url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<sid>\d+)/$', views.detail, name='detail'),
-    url(r'^add/$', views.create, name='create'),
+    url(r'^add/$', views.create, name='add'),
     url(r'^remove/(?P<sid>\d+)/$', views.remove, name='remove'),
     url(r'^edit/(?P<sid>\d+)/$', views.edit, name='edit'),
 )
