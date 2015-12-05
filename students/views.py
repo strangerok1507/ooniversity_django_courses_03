95a614b0948efdcef01303ad14ebde989c49be1c
Vlad Zhirov <zhirov0vlad@gmail.com>
2015-12-05 17:57:15 +0200

    change text for the views.py

--- students/views.py
+++ students/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render,redirect
 from students.models import Student
 from courses.models import Course
-from students.forms import StudentForm
+from students.forms import StudentModelForm
 from django.contrib import messages
 
 
@@ -30,12 +30,12 @@ def detail(request,sid):
 
 
 def create(request):
-  form = StudentForm()
+  form = StudentModelForm()
   if request.method == 'POST':
-    form = StudentForm(request.POST)
+    form = StudentModelForm(request.POST)
     if form.is_valid():
       add_stud = form.save()
-      messages.success(request, u'Студент %s %s успешно добавлен.' % (add_stud.name, add_stud.surname))
+      messages.success(request, u'Student %s %s has been successfully added.' % (add_stud.name, add_stud.surname))
       return redirect('students:list_view')
   else:
     return render(request,'students/add.html',{'form':form})
@@ -45,18 +45,18 @@ def remove(request,sid):
   message = u"%s %s" %(student.name, student.surname)
   if request.method == 'POST':
     student.delete()
-    messages.success(request, u"Студент %s %s успешно удален." % (student.name, student.surname))
+    messages.success(request, u"Info on %s %s has been sucessfully deleted." % (student.name, student.surname))
     return redirect('students:list_view')
   return render(request, 'students/remove.html', {'message': message})
 
 def edit(request,sid):
   student = Student.objects.get(id=sid)
   if request.method == 'POST':
-    form = StudentForm(request.POST, instance=student)
+    form = StudentModelForm(request.POST, instance=student)
     if form.is_valid():
       form.save()
-      messages.success(request, u'Информация о студенте успешно обновлена.')
+      messages.success(request, u'Info on the student has been sucessfully changed.')
       return redirect('students:list_view')
   else:
-    form = StudentForm(instance=student)
+    form = StudentModelForm(instance=student)
     return render(request,'students/edit.html',{'form':form})
